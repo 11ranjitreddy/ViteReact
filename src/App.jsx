854f1a87b7842a33492bbadfe2ee4eb5e0dc3aca
ranjit <ranjithredd8479@gmail.com>
2025-09-16 23:09:37 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <h1>Ranitreddy</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
